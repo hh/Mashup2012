c3818446a6adc08a07e5cee213529c86e335c0e8
Chris McClimans <chris@hippiehacker.org>
2012-03-31 18:19:17 +1300

    made simple web interface

--- config/init.rb
+++ config/init.rb
@@ -1,19 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
-require 'sinatra/sequel'
-require 'sqlite3'
+# require 'sinatra/sequel'
+# require 'sqlite3'
 
-db_path = File.expand_path(File.join(File.dirname(__FILE__), '../db', "#{ENV['OPSCODE_ENV']}frankenspice.db"))
-set :database, "sqlite://#{db_path}"
+# db_path = File.expand_path(File.join(File.dirname(__FILE__), '../db', "mashup2012.db"))
+# set :database, "sqlite://#{db_path}"
 
-migration "create the audit_log table" do
-  database.create_table :audit_log do
-    primary_key :id
-    datetime    :created_at
-    text        :apps
-    text        :version
-    text        :user
-    text        :reason
-  end
-end
+# migration "create the audit_log table" do
+#   database.create_table :audit_log do
+#     primary_key :id
+#     datetime    :created_at
+#     text        :apps
+#     text        :version
+#     text        :user
+#     text        :reason
+#   end
+# end
--- frontend/frontend.rb
+++ frontend/frontend.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
-require 'sinatra/sequel'
 require File.expand_path('../../config/init',  __FILE__)
 
 require 'open-uri'
